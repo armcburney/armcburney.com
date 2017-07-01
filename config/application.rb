16565e07ea1643b757ec94ba019915df7440bbaa
Andrew R. McBurney <andrewrobertmcburney@gmail.com>
2017-07-01 11:11:18 -0700

    Added vitalcss, updated home views, created nav partial.

--- config/application.rb
+++ config/application.rb
@@ -9,6 +9,9 @@ module AndrewMcBurney
     config.load_defaults 5.1
     config.sass.preferred_syntax = :sass
 
+    config.assets.enabled = true
+    config.assets.paths << Rails.root.join('app', 'assets', 'pdfs')
+
     config.generators do |g|
       g.template_engine :slim
       g.test_framework :rspec
